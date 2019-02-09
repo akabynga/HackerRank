ed3f05516a25a5eadfe6fe63d26eba8e107a904d
Dzmitry Shchepin <Dzmitry.Shchepin@gmail.com>
2019-02-10 01:47:01 +0300

    Bug fixing

--- src/com/algorithms/search/IceCreamParlor.java
+++ src/com/algorithms/search/IceCreamParlor.java
@@ -20,7 +20,7 @@ public class IceCreamParlor {
 		int[] result = new int[2];
 
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = i; j < arr.length; j++) {
+			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] + arr[j] == m) {
 					result[0] = i + 1;
 					result[1] = j + 1;
